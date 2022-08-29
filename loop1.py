5300fa8de9ffd8a487b51681c702061c8271d893
HARIKRISHNA-FSD-1908 <112386854+HARIKRISHNA-FSD-1908@users.noreply.github.com>
2022-08-29 17:11:32 +0530

    Add files via upload

---
+++ loop1.py
@@ -0,0 +1,8 @@
+a=int(input())
+c=0
+product=1
+while c<a:
+    b=int(input())
+    c=c+1
+    product=product*b
+print(product)   
---
+++ sai.py
@@ -0,0 +1,3 @@
+a=input()
+for x in range(1,len(a)+1):
+    print(a[-x])
